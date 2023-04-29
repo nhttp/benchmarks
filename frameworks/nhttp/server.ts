8b5446a95e20e28e4aec9a1148077c622b0ee2cf
herudi <herudi7@gmail.com>
2023-04-29 13:58:13 +0700

    Update server.ts

--- frameworks/nhttp/server.ts
+++ frameworks/nhttp/server.ts
@@ -1,12 +1,12 @@
 import { nhttp } from "https://deno.land/x/nhttp/mod.ts";
 
-const app = nhttp()
+nhttp({ flash: true })
   .get("/", () => "home")
   .get("/blog/:id", (rev) => `${rev.params.id} ${rev.query.title}`)
   .get("/api/user", (rev) => {
     rev.response.setHeader("x-powered-by", "bench");
     return { user: "john" };
-  });
-  
- await Deno.serve({ port: 8000 }, app.handle); 
+  })
+  .listen(8000);
+
 
