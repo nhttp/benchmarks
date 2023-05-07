f98b864586fc0401c6f8c7e0b7bf9acd8ec4cdf8
herudi <herudi7@gmail.com>
2023-05-07 19:48:25 +0700

    Update server.ts

--- frameworks/nhttp/server.ts
+++ frameworks/nhttp/server.ts
@@ -1,4 +1,4 @@
-import { nhttp } from "https://deno.land/x/nhttp/mod.ts";
+import { nhttp } from "https://raw.githubusercontent.com/nhttp/nhttp/master/mod.ts";
 
 nhttp({ flash: true })
   .get("/", () => "home")
