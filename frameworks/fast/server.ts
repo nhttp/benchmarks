3f8d8dc5baca94ac87e4cfec97f5a6488877c376
herudi <herudi7@gmail.com>
2023-09-01 13:58:44 +0700

    fix fast deno

--- frameworks/fast/server.ts
+++ frameworks/fast/server.ts
@@ -11,4 +11,4 @@ const app = fast()
     return Response.json({ user: "john" }, { headers });
   });
 
-await Deno.serve({ port: 8000 }, (req) => app.handle(req));
+Deno.serve({ port: 8000 }, (req) => app.handle(req));
